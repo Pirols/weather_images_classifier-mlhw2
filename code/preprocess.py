08853b9f7c9d4c001fe16320ce95d01a0a1390b7
Edoardo Piroli <piroliedoardo@gmail.com>
2019-11-30 18:05:14 +0100

    Completes preprocess.py

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,17 +1,18 @@
 # Used to navigate directories
 import os
 
-# Used to process the pictures
-from PIL import Image, ImageOps
+# Used to process and save the pictures
+from PIL import Image
 
-# Used to save the new images
-from matplotlib import pyplot as plt
+# Used to randomly sample training and testing datasets
+import numpy as np
 
-def resize_picture(picture_path: str, desired_size: int, output_path: str) -> None:
+
+def resize_picture(picture, desired_size, output_path):
     """
-        resize_picture(picture_path: str, desired_size: int, output_path: str) -> None
+        resize_picture(picture, desired_size, output_path)
         takes as INPUTS:
-            -picture_path: the path to an image file
+            -picture_path: an image file, usually opened via PIL.Image.open(path)
             -desired_size: the size of the square picture we want to fit our resized picture in
             -output_path: the path to store the resized picture
         DOES:
@@ -24,32 +25,34 @@ def resize_picture(picture_path: str, desired_size: int, output_path: str) -> No
         This code was taken from https://jdhao.github.io/2017/11/06/resize-image-to-square-with-padding/
     """
 
-    try:
-        # try-except clause used to check if the path is a valid picture file
-        im = Image.open(picture_path)
-    except IOError:
-        return
-    
-    old_size = im.size
+    old_size = picture.size
 
     ratio = float(desired_size)/max(old_size)
     new_size = tuple([int(x*ratio) for x in old_size])
 
-    im = im.resize(new_size, Image.ANTIALIAS)
+    im = picture.resize(new_size, Image.ANTIALIAS)
 
     new_im = Image.new("RGB", (desired_size, desired_size))
     new_im.paste(im, ((desired_size-new_size[0])//2, (desired_size-new_size[1])//2))
 
-    plt.imshow(new_im)
-    plt.savefig(output_path)
-    
+    new_im.save(output_path)
+
 
 if __name__ == "__main__":
 
-    dataset_path = './original_dataset/MWI-Dataset-1.1_2000'
-    dataset_path = './resized_dataset/MWI-Dataset-1.1_2000'
+    dataset_path = './datasets/MWI-Dataset-1.1_2000'
+    new_dataset_path = './datasets/parsed_MWI'
+
+    DESIRED_SIZE = 160
 
     for subdir, dirs, files in os.walk(dataset_path):
         for file in files:
 
-            
+            try:
+                pic = Image.open(os.path.join(subdir, file))
+            except IOError:
+                continue
+
+            output = os.path.join(new_dataset_path + ('/training' if np.random.uniform() <= 0.85 else '/testing') + subdir[subdir.rfind('/'):], file[:file.rfind('.')] + '_resized' + file[file.rfind('.'):])
+            resize_picture(pic, DESIRED_SIZE, output)
+    
