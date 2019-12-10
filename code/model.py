d4ce5c9eea5eb63d1f09a12a1b5187e935ca7813
Edoardo Piroli <piroliedoardo@gmail.com>
2019-12-10 10:14:00 +0100

    Adds fine_tune_at parameter to build_model

--- code/model.py
+++ code/model.py
@@ -1,11 +1,14 @@
 from tensorflow import keras as K
 
-def build_model(input_shape, output_size, weights='imagenet', dense_size=64, regularization_factor=0.001, dropout_rate=0.3, base_learning_rate=0.001):
+def build_model(input_shape, output_size, fine_tune_at=None, weights='imagenet', dense_size=64, regularization_factor=0.001, dropout_rate=0.3, base_learning_rate=0.001):
     """
         PARAMETERS:
             -input_shape: The shape of the model's input, e.g. (299, 299, 3)
             -output_size: The number of output classes the model will be trained to classify
-            -weights[Default:'imagenet']: Should be either imagenet or None for pretrained or nonpretrained xception model.
+            -fine_tune_at[Default: None]: Only valid if weights='imagenet', it represents the number of layers which should be frozen, i.e. not fine-tuned.
+                                          It can also take negative values, e.g. -1 -> only the last layer will be left trainable.
+                                          If Falsey the whole pretrained model will be frozen
+            -weights[Default: 'imagenet']: Should be either imagenet or None for pretrained or nonpretrained xception model.
             -dense_size[Default: 64]: The size of the first dense layer
             -regularization_factor[Default: 0.001]: The parameters of the L2 regularization on the first dense layer
             -dropout_rate[Default: 0.3]: The rate of the dropout layers
@@ -18,15 +21,24 @@ def build_model(input_shape, output_size, weights='imagenet', dense_size=64, reg
                 3) Dropout layer
                 4) Pooling layer
                 5) Dropout layer
-                6) Output - dense - layer
+                6) Output (dense) layer
 
         RAISES:
             -
     """
 
-    base_model = K.applications.MobileNetV2(input_shape=input_shape, include_top=False, weights=weights)
+    # Gather the base model
+    base_model = K.applications.xception.Xception(input_shape=input_shape, include_top=False, weights=weights)
 
-    # Complete the model with an average pooling and a final dense layer
+    # Freeze a part of or the whole base_model
+    if weights == 'imagenet':
+        if fine_tune_at:
+            for layer in base_model.layers[:fine_tune_at]:
+                layer.trainable = False
+        else:
+            base_model.trainable = False
+
+    # Complete the model with other layers
     model = K.Sequential([
         base_model,
         K.layers.Dense(dense_size, kernel_regularizer=K.regularizers.l2(regularization_factor), bias_regularizer=K.regularizers.l2(regularization_factor), activation='relu'),
@@ -36,8 +48,9 @@ def build_model(input_shape, output_size, weights='imagenet', dense_size=64, reg
         K.layers.Dense(output_size, activation='softmax')
     ])
 
+    # Compile the model
     model.compile(optimizer=K.optimizers.RMSprop(learning_rate=base_learning_rate, rho=0.9),
-                loss='categorical_crossentropy',
-                metrics=['categorical_accuracy'])
+                  loss='categorical_crossentropy',
+                  metrics=['categorical_accuracy'])
 
     return model
