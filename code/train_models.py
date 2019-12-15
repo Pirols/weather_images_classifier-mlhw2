12eaf5ac9f7f97ef50f352a9b77c15e5927c7380
Edoardo Piroli <piroliedoardo@gmail.com>
2019-12-15 22:27:47 +0100

    Finished train_models.py

--- code/train_models.py
+++ code/train_models.py
@@ -1,4 +1,4 @@
-from model import build_model
+from model import build_pretrained_model, AlexNet
 from plot import save_plot
 
 # Used to count the number of samples
@@ -63,6 +63,12 @@ if __name__ == "__main__":
                                                                             class_mode="categorical",
                                                                             shuffle=True)
 
+    test_generator   = ImageDataGenerator(rescale=1./255).flow_from_directory(testing_dataset,
+                                                                            target_size=(IMG_SIZE, IMG_SIZE),
+                                                                            color_mode="rgb",
+                                                                            batch_size=BATCH_SIZE,
+                                                                            class_mode="categorical")
+
     # dictionaries used to store the histories of training of both the models over the gridsearch run
     histories = dict()
 
@@ -76,65 +82,56 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
 
     ### Performing a very simple GridSearch ###
-    for weights in ['imagenet', None]:
-        for lr in learning_rates:
-            for drop in dropout_rates:
-                for reg in regularizer_rates:
-                    for size in dense_sizes:
-                        # build the  model
-                        model = build_model(INPUT_SHAPE, 
-                                            OUTPUT_CLASSES,
-                                            weights=weights,
-                                            dense_size=size,
-                                            regularization_factor=reg,
-                                            dropout_rate=drop,
-                                            base_learning_rate=lr)
+    for lr in learning_rates:
+        for drop in dropout_rates:
+            for reg in regularizer_rates:
+                for size in dense_sizes:
+                    # build the  model
+                    model = build_pretrained_model(INPUT_SHAPE, 
+                                                OUTPUT_CLASSES,
+                                                dense_size=size,
+                                                regularization_factor=reg,
+                                                dropout_rate=drop,
+                                                base_learning_rate=lr)
 
-                        # train the model
-                        comb  = (weights, lr, drop, reg, size)
-                        histories[comb] = model.fit_generator(train_generator,
-                                                                steps_per_epoch=TRAINING_STEPS,
-                                                                epochs=EPOCHS,
-                                                                validation_data=val_generator, 
-                                                                validation_steps=VALIDATION_STEPS,
-                                                                callbacks=[es_val_loss])
+                    # train the model
+                    comb  = (lr, drop, reg, size)
+                    histories[comb] = model.fit_generator(train_generator,
+                                                            steps_per_epoch=TRAINING_STEPS,
+                                                            epochs=EPOCHS,
+                                                            validation_data=val_generator, 
+                                                            validation_steps=VALIDATION_STEPS,
+                                                            callbacks=[es_val_loss])
 
-                        # update best combination of hyperparameters based on validation loss
-                        if minimum > min(histories[comb].history['val_loss']):
-                            minimum = min(histories[comb].history['val_loss'])
-                            best_comb = comb
+                    # update best combination of hyperparameters based on validation loss
+                    if minimum > min(histories[comb].history['val_loss']):
+                        minimum = min(histories[comb].history['val_loss'])
+                        best_comb = comb
 
-                        # save plots
-                        save_path = './plots/' + '_'.join(str(comb[i]) for i in range(len(comb))) + '.pdf'
-                        save_plot(save_path, histories[comb].history)
+                    # save plots
+                    save_path = './plots/' + '_'.join(str(comb[i]) for i in range(len(comb))) + '.pdf'
+                    save_plot(save_path, histories[comb].history)
 
-                        # storying histories for later usages
-                        with open('./histories/'+'_'.join(str(label) for label in comb), 'wb') as fd:
-                            pickle.dump(histories[comb].history, fd)
+                    # storying histories for later usages
+                    with open('./histories/'+'_'.join(str(label) for label in comb), 'wb') as fd:
+                        pickle.dump(histories[comb].history, fd)
 
-                        if not weights:
-                            print('Trained from scratch with: lr={}, dropout={}, regularization term={}, dense size={}'.
-                                format(lr, drop, reg, size))
-                            # fine_tune_at doesn't affect the non-pretrained model
-                            break
-                        else:
-                            print('Trained from pretrained with: lr={}, dropout={}, regularization term={}, dense size={}'.
-                                format(lr, drop, reg, size))
+                    print('Trained from scratch with: lr={}, dropout={}, regularization term={}, dense size={}'.
+                        format(lr, drop, reg, size))
 
     print('The best combination of hyperparameters is found to be: {}'.format(best_comb))
     print('The corresponding metrics are: val_loss={} and val_categorical_accuracy={}'.
         format(minimum, histories[best_comb].history['val_categorical_accuracy'][np.argmin(histories[best_comb].history['val_loss'])]))
 
-    weights, lr, drop, reg, size = best_comb
+    lr, drop, reg, size = best_comb
 
     ### Train the best model ###
-    best_model = build_model(INPUT_SHAPE, 
-                             OUTPUT_CLASSES,
-                             weights=weights,
-                             dense_size=size,
-                             regularization_factor=reg,
-                             dropout_rate=drop,
-                             base_learning_rate=lr)
+    best_model = build_pretrained_model(INPUT_SHAPE, 
+                                        OUTPUT_CLASSES,
+                                        dense_size=size,
+                                        regularization_factor=reg,
+                                        dropout_rate=drop,
+                                        base_learning_rate=lr)
 
     checkpointer = K.callbacks.ModelCheckpoint('weights.{epoch:02d}-{val_loss:.2f}.hdf5',
                                                monitor='val_loss',
@@ -147,7 +144,26 @@ if __name__ == "__main__":
                             validation_steps=VALIDATION_STEPS,
                             callbacks=[es_val_loss, checkpointer])
 
+    print('Evaluating the pre-trained model on the testing dataset yielded the following results: {}'.
+            format(best_model.evaluate_generator(test_generator)))
+
+    # New Model
+    new_model = AlexNet(INPUT_SHAPE, OUTPUT_CLASSES, 0.0001)
+
+    history = new_model.fit_generator(train_generator,
+                                        steps_per_epoch=TRAINING_STEPS,
+                                        epochs=EPOCHS,
+                                        validation_data=val_generator, 
+                                        validation_steps=VALIDATION_STEPS,
+                                        callbacks=[es_val_loss])
+
+    # save plots
+    save_path = './plots/' + 'new_model.pdf'
+    save_plot(save_path, history.history)
+
+    print('Evaluating the new model on the testing dataset yielded the following results: {}'.
+            format(new_model.evaluate_generator(test_generator)))
+
     # re-enabling warnings
     warnings.filterwarnings("default")
-
-    ### Test the final model on the testing dataset
+    
